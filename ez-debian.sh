3b441068c31c02d1989c6632ca771be4880c034d
arxad2374 <118026260+arxad2374@users.noreply.github.com>
2023-06-15 11:51:16 +0600

    Update ez-debian.sh

--- ez-debian.sh
+++ ez-debian.sh
@@ -6,37 +6,36 @@ apt upgrade -y
 
 # Install necessary packages
 apt install -y sudo wget htop neofetch iproute2 net-tools iptables
-apt --fix-broken install
-
-# Download and install Docker dependencies
-docker_compose_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-compose-plugin_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
-docker_ce_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-ce_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
-docker_ce_cli_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-ce-cli_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
-docker_buildx_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-buildx-plugin_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
-containerd_io_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"containerd.io_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
 
 # Set the PATH variable with common directories
 export PATH="/usr/local/sbin:/usr/sbin:/sbin:$PATH"
 
+containerd_io_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"containerd.io_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
 wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${containerd_io_url}"
 dpkg -i "${containerd_io_url}"
+rm -f "${containerd_io_url}"
+
+docker_ce_cli_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-ce-cli_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
 wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_ce_cli_url}"
 dpkg -i "${docker_ce_cli_url}"
+rm -f "${docker_ce_cli_url}"
+
+docker_ce_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-ce_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
 wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_ce_url}"
 dpkg -i "${docker_ce_url}"
+rm -f "${docker_ce_url}"
+
+docker_buildx_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-buildx-plugin_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
 wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_buildx_url}"
 dpkg -i "${docker_buildx_url}"
+rm -f "${docker_buildx_url}"
+
+docker_compose_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-compose-plugin_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
 wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_compose_url}"
 dpkg -i "${docker_compose_url}"
+rm -f "${docker_compose_url}"
 
 systemctl enable --now docker
-
-# Cleanup
-rm -f "${docker_compose_url}"
-rm -f "${docker_ce_url}"
-rm -f "${docker_ce_cli_url}"
-rm -f "${docker_buildx_url}"
-rm -f "${containerd_io_url}"
 
 echo "Script execution completed."
 

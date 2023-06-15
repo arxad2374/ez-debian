f4d2f21c3e01963fe5899d366a27dd04735a82fd
arxad2374 <118026260+arxad2374@users.noreply.github.com>
2023-06-15 11:28:01 +0600

    Update ez-debian.sh

--- ez-debian.sh
+++ ez-debian.sh
@@ -15,11 +15,11 @@ docker_ce_cli_url=$(wget -qO- https://download.docker.com/linux/debian/dists/boo
 docker_buildx_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"docker-buildx-plugin_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
 containerd_io_url=$(wget -qO- https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/ | grep -oP "href=\"containerd.io_[^\"]+\.deb\"" | awk -F '"' '{print $2}' | sort -V | tail -1)
 
-wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_compose_url}"
+wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${containerd_io_url}"
 wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_ce_url}"
 wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_ce_cli_url}"
 wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_buildx_url}"
-wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${containerd_io_url}"
+wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/${docker_compose_url}"
 
 # Set the PATH variable with common directories
 export PATH="/usr/local/sbin:/usr/sbin:/sbin:$PATH"
